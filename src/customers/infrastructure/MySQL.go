f19c5e5f6c228be75396d9a6f3f4528f225650e4
LimbergDV <limbergdelvalle@gmail.com>
2025-02-05 23:58:08 -0600

    add: adding infrastructure of customers

--- src/customers/infrastructure/MySQL.go
+++ src/customers/infrastructure/MySQL.go
@@ -1,1 +1,18 @@
 package infrastructure
+
+import (
+	core "api_short_long_polling/src/Core"
+	"log"
+)
+
+type MySQL struct {
+	conn *core.Conn_MySQL
+}
+
+func NewMySQL() *MySQL {
+	conn := core.GetDBPool()
+	if conn.Err != "" {
+		log.Fatalf("Error al configurar el pool de conexiones: %v", conn.Err)
+	}
+	return &MySQL{conn: conn}
+}
