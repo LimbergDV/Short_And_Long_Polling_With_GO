a6f234b4ad59735bf32c8cee7a6e08630bac87fa
LimbergDV <limbergdelvalle@gmail.com>
2025-02-05 23:56:33 -0600

    add: finished domain of customers

--- src/customers/domain/ICustomer_repository.go
+++ src/customers/domain/ICustomer_repository.go
@@ -1,1 +1,10 @@
 package domain
+
+//Separa el modelo de la capa de negocio
+//que necesitan hacer mis actores con los casos de uso
+type ICustomer interface {
+	Save(customers Customer) (uint, error) //podemos retornar algo acá, como un string, etc.
+	GetAll() ([]Customer) //tenemos nuestros métodos los cuales los usuarios hacen la acción con nuestra base de datos
+	Delete(id int) (uint, error)
+	Update(id int, customer Customer) (uint, error)
+}
--- src/customers/domain/customer.go
+++ src/customers/domain/customer.go
@@ -1,1 +1,10 @@
 package domain
+
+type Customer struct {
+	Id int
+	Name string
+	Last_name string
+	Phone_number string
+	Curp string
+	Number_license string
+}
